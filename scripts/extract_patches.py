084fd17ad2bddcd8ed4d0b03c3677eb1fe6c10a8
Jacob Vanluven <jvanluven14@gmail.com>
2026-07-20 12:58:32 -0400

    Updated Extraction Pipeline
    Updated the extraction script to support a few arguments rather than be hardcoded. This change now has the entire HistoricalWI dataset being extracted and stored in both the testing and training set

--- scripts/extract_patches.py
+++ scripts/extract_patches.py
@@ -1,3 +1,6 @@
+import os
+from argparse import ArgumentParser
+
 import torch
 from torch.utils.data import DataLoader
 
@@ -6,11 +9,22 @@ from tqdm import tqdm
 from writer_retrieval.data import TO_FLOAT, NORMALIZE
 from writer_retrieval.data.dataset import HistoricalWIDataset, WindowSampler, window_collate
 from writer_retrieval.data.serialization import save_patches
-from writer_retrieval.models.dino import DINOModelv3
+from writer_retrieval.models.dino import *
 from writer_retrieval.data.filter import get_window_filter, get_patch_filter
 
-if __name__ == "__main__":
-    dataset = HistoricalWIDataset("datasets/historical_wi/test")
+def extract_dataset(
+        dataset_root: str,
+        output_name: str,
+        weight_path: str,
+        dino_version: str,
+        use_dino_v1: bool = False,
+    ) -> None:
+    
+    """
+    Extracts a collection of images using DINO
+    """
+    
+    dataset = HistoricalWIDataset(dataset_root)
     
     dataloader = DataLoader(
         dataset,
@@ -20,7 +34,12 @@ if __name__ == "__main__":
     )
     
     # load dino model
-    model = DINOModelv3("dinov3_vits16", "weights/dinov3_vits16", False)
+    model: DINOModelBase
+    
+    if use_dino_v1:
+        model = DINOModelv1(dino_version, weight_path, False)
+    else:
+        model = DINOModelv3(dino_version, weight_path, False)
     
     # start processing
     data = tqdm(dataloader)
@@ -80,5 +99,42 @@ if __name__ == "__main__":
         })
         
         # store tokens
-        save_patches(f"output/patches/patch_{iteration}.pt", tokens, writers, documents)
+        save_patches(f"output/patches/{output_name}/patch_{iteration}.pt", tokens, writers, documents)
         iteration += 1
+
+if __name__ == "__main__":
+    parser = ArgumentParser("Extract Patches")
+    
+    # add arguments
+    parser.add_argument("dataset")
+    parser.add_argument("run_name")
+    parser.add_argument("-w", "--weights", default="weights/dinov3_vits16")
+    parser.add_argument("-v", "--version", default="dinov3_vits16")
+    parser.add_argument("--dino-v1", default=False, action="store_true")
+    
+    # parse arguments
+    args = parser.parse_args()
+    
+    # print settings
+    print(f"Extracting dataset: `{args.dataset}` ({args.run_name})")
+    print(f"    Weights: `{args.weights}`")
+    print(f"    Version: `{args.version}`")
+    print(f"    Model:   `{"DINOv1" if args.dino_v1 else "DINOv3"}`")
+    
+    # extract training set
+    extract_dataset(
+        os.path.join(args.dataset, "train"),
+        os.path.join(args.run_name, "train"),
+        weight_path=args.weights,
+        dino_version=args.version,
+        use_dino_v1=args.dino_v1,
+    )
+    
+    # extract testing set
+    extract_dataset(
+        os.path.join(args.dataset, "test"),
+        os.path.join(args.run_name, "test"),
+        weight_path=args.weights,
+        dino_version=args.version,
+        use_dino_v1=args.dino_v1,
+    )
